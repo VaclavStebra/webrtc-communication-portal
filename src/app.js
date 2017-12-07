a9fa93332b456ee96649fece7c75ae26d36adcc0
Vaclav Stebra <vstebra@gmail.com>
2017-12-07 21:28:42 +0100

    Material UI

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import Root from './components/Root';
+
+const App = () => (
+  <MuiThemeProvider>
+    <Root />
+  </MuiThemeProvider>
+);
 
 const mountPoint = document.getElementById('react');
 
 ReactDOM.render(
   (
-    <Root />
+    <App />
   ),
   mountPoint
 );
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import RaisedButton from 'material-ui/RaisedButton';
+
 export default class HomePage extends React.Component {
   render() {
     return (
       <div>
         <h1>/ route</h1>
         <Link to="/test">
-          Test
+          <RaisedButton label="Test" primary />
         </Link>
       </div>
     );
