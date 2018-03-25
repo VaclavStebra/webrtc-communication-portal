88f069a0e8f1f9b693e0e841454fc35bc1a1d6cc
Vaclav Stebra <vstebra@gmail.com>
2018-03-25 20:14:20 +0200

    Error handling

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,36 +4,46 @@ const router = express.Router();
 const UserManager = require('../modules/user/UserManager');
 const UserToken = require('../modules/user/UserToken');
 
-router.post('/token', async function (req, res) {
+router.post('/token', async function (req, res, next) {
   const { email, password } = req.body;
 
   const userManager = new UserManager(email, password);
-  const user = await userManager.getUser();
 
-  if (!user) {
-    return res.status(401).json({ error: 'Invalid credentials' });
+  try {
+    const user = await userManager.getUser();
+
+    if (!user) {
+      return next({ status: 401, message: 'Invalid credentials'});
+    }
+
+    const tokenManager = new UserToken(user.email);
+    const token = tokenManager.getToken();
+
+    return res.json({ token });
+  } catch (ex) {
+    return next(ex);
   }
-
-  const tokenManager = new UserToken(user.email);
-  const token = tokenManager.getToken();
-
-  return res.json({ token });
 });
 
-router.post('/signup', async function (req, res) {
+router.post('/signup', async function (req, res, next) {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(422).json({ error: 'Invalid credentials '});
+    return next({ status: 422, message: 'Invalid credentials' });
   }
 
   const userManager = new UserManager(email, password);
-  await userManager.createUser();
 
-  const tokenManager = new UserToken(email);
-  const token = tokenManager.getToken();
+  try {
+    await userManager.createUser();
 
-  return res.json({ token });
+    const tokenManager = new UserToken(email);
+    const token = tokenManager.getToken();
+
+    return res.json({ token });
+  } catch (ex) {
+    return next(ex);
+  }
 });
 
 module.exports = router;
